2ed2cde8d8ca5bcf47e9c1c4beb16b240c8cc099
Aroxed <petrashenko@gmail.com>
2023-12-08 15:39:22 +0200

    initial

---
+++ src/components/history.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>The list:</h1>
+        <p v-for="value in valueList">{{ value }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "History",
+        props: ["valueList"]
+    }
+</script>
